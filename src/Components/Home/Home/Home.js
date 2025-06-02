657f140fbd223f85feb083d0de078436f370e7c2
abbosaliboev <abbosaliboyev66669999@gmail.com>
2025-06-03 02:40:36 +0900

    Remove scroll

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,39 +1,24 @@
-// Home.js (with Locomotive Scroll integration)
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
 import Pricing from '../Pricing/Pricing';
 import Services from '../Services/Services';
-import LocomotiveScroll from 'locomotive-scroll';
-import 'locomotive-scroll/dist/locomotive-scroll.css';
+
+ 
 
 const Home = () => {
-  const scrollRef = useRef(null);
+    return (
+       <>
+      
+       <Banner></Banner>
+       <About></About>
+       <Services></Services>
+       <Pricing></Pricing>
+       <Contact></Contact>
 
-  // Initialize Locomotive Scroll
-  useEffect(() => {
-    const scroll = new LocomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-      lerp: 0.08, 
-      multiplier: 1.2 
-    });
-
-    return () => {
-      scroll.destroy();
-    };
-  }, []);
-
-  return (
-    <div data-scroll-container ref={scrollRef}>
-      <Banner />
-      <About />
-      <Services />
-      <Pricing />
-      <Contact />
-    </div>
-  );
+       </>
+    );
 };
 
 export default Home;
